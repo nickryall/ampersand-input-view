f8dd0bea172ee5d2061b84ae7b07b257ef22cfba
nickryall <me@nickryall.com.au>
2015-02-02 11:14:56 +1300

    Remove skipValidation argument from reset method, pass true for skipValidation on setValue

--- ampersand-input-view.js
+++ ampersand-input-view.js
@@ -209,8 +209,8 @@ module.exports = View.extend({
     reset: function () {
         this.setValue(this.startingValue);
     },
-    clear: function (skipValidation) {
-        this.setValue('', skipValidation);
+    clear: function () {
+        this.setValue('', true);
     },
     reportToParent: function () {
         if (this.parent) this.parent.update(this);
